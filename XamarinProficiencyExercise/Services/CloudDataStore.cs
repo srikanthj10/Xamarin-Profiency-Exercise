4308e69f51f5b712e1ec8e5c220ea3c69983ce77
srikanthj10 <srikanthj10@gmail.com>
2018-02-12 18:03:16 +0530

    Update CloudDataStore.cs

--- XamarinProficiencyExercise/Services/CloudDataStore.cs
+++ XamarinProficiencyExercise/Services/CloudDataStore.cs
@@ -48,52 +48,5 @@ namespace XamarinProficiencyExercise
             }
             return items;
         }
-
-        public async Task<Item> GetItemAsync(string id)
-        {
-            if (id != null && CrossConnectivity.Current.IsConnected)
-            {
-                var json = await client.GetStringAsync($"api/item/{id}");
-                return await Task.Run(() => JsonConvert.DeserializeObject<Item>(json));
-            }
-
-            return null;
-        }
-
-        public async Task<bool> AddItemAsync(Item item)
-        {
-            if (item == null || !CrossConnectivity.Current.IsConnected)
-                return false;
-
-            var serializedItem = JsonConvert.SerializeObject(item);
-
-            var response = await client.PostAsync($"api/item", new StringContent(serializedItem, Encoding.UTF8, "application/json"));
-
-            return response.IsSuccessStatusCode;
-        }
-
-        public async Task<bool> UpdateItemAsync(Item item)
-        { // || item.Id == null
-            if (item == null || !CrossConnectivity.Current.IsConnected)
-                return false;
-
-            var serializedItem = JsonConvert.SerializeObject(item);
-            var buffer = Encoding.UTF8.GetBytes(serializedItem);
-            var byteContent = new ByteArrayContent(buffer);
-
-            var response = await client.PutAsync(new Uri($"api/item/{item.title}"), byteContent);
-
-            return response.IsSuccessStatusCode;
-        }
-
-        public async Task<bool> DeleteItemAsync(string id)
-        {
-            if (string.IsNullOrEmpty(id) && !CrossConnectivity.Current.IsConnected)
-                return false;
-
-            var response = await client.DeleteAsync($"api/item/{id}");
-
-            return response.IsSuccessStatusCode;
-        }
     }
 }
