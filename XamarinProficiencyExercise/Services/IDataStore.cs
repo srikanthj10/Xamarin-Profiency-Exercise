672b4611179c06515b131c18a0ee274db955ea98
srikanthj10 <srikanthj10@gmail.com>
2018-02-12 18:02:27 +0530

    Update IDataStore.cs

--- XamarinProficiencyExercise/Services/IDataStore.cs
+++ XamarinProficiencyExercise/Services/IDataStore.cs
@@ -5,7 +5,6 @@ namespace XamarinProficiencyExercise
 {
     public interface IDataStore<T>
     {
-        Task<bool> AddItemAsync(T item);
         Task<T> GetItemsAsync(bool forceRefresh = false);
         Task<T> GetItemsAsync(string SortOrder);
     }
