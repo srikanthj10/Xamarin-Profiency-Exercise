5c57fbcac699bc9b782e9cf9d2151185403909ee
srikanthj10 <srikanthj10@gmail.com>
2018-02-23 20:44:47 +0530

    Update Constants.cs
    Change the production json url

--- XamarinProficiencyExercise/Assets/Constants.cs
+++ XamarinProficiencyExercise/Assets/Constants.cs
@@ -3,8 +3,8 @@ namespace XamarinProficiencyExercise.Assets
 {
     public class Constants
     {
-        //public static string BackendUrl = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json";
-        public static string BackendUrl = "http://www.mocky.io/v2/5a8a680d2f00006200e46f7c";
+        public static string BackendUrl = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json";
+//         public static string BackendUrl = "http://www.mocky.io/v2/5a8a680d2f00006200e46f7c";
         public static string NoImageURL = "https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF";
         public static string NoDescription = "No descriptions available";
         public static string NoTitle = "No Title available";
